2922c31a04b179408b50fa386ec3f5a2cf343aca
Alejandro González Reyes <alexglzrys@gmail.com>
2022-09-03 11:12:58 -0500

    crear un listado de categorías

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -1,5 +1,14 @@
+import { useState } from "react";
+
 // Componente global de la aplicación
 const GiftExpertApp = () => {
+  // El estado no se puede condicionar con IF's
+  const [categories, setCategories] = useState([
+    "Dragon Ball",
+    "Pokemón",
+    "Ranma 1/2",
+  ]);
+
   return (
     <>
       {/* Titulo */}
@@ -7,6 +16,12 @@ const GiftExpertApp = () => {
 
       {/* Buscador */}
       {/* Listado de Gifts */}
+      <ol>
+        {/* Iterar e imprimir un listado de elementos */}
+        {categories.map((category, index) => (
+          <li key={index}>{category}</li>
+        ))}
+      </ol>
       {/* Gift Item */}
     </>
   );
