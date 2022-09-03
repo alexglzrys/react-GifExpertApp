cbc4e652b765cdb768e212a50a7f1dd9b4322f7f
Alejandro González Reyes <alexglzrys@gmail.com>
2022-09-03 16:31:58 -0500

    componente GifItem

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
+import { GifItem } from "./GifItem";
 
 // Sniped para crear el cuerpo de un componente funcional - rafc
 export const GifGrid = ({ category }) => {
@@ -19,9 +20,10 @@ export const GifGrid = ({ category }) => {
   return (
     <div>
       <h4>{category}</h4>
-      <ol>
-        {images.map(({id, title}) => <li key={id}>{title}</li>)}
-      </ol>
+      <div className="card-grid">
+        {/* Tarjetas con la información de cada gif localizado referente a la categoría seleccionada */}
+        {images.map(image => <GifItem key={image.id} {...image}/>)}
+      </div>
     </div>
   );
 };
